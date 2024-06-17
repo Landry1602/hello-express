c9f5dab8528e523671fc7dfe153290c2a15b4023
Landry1602 <landrymaillard@gmail.com>
2024-06-17 10:58:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+app
+  .listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error:", err.message);
+  });
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my favourite movie list");
+});
+
+app.get("/api/movies", (req, res) => {
+    res.status(200).json(movies);
+});
+
+app.get("/api/movies/:id", (req, res) => {
+    const movieId = parseInt(req.params.id, 10); 
+    const movie = movies.find(m => m.id === movieId);
+
+    if (movie) {
+        res.status(200).json(movie)
+    } else {
+        console.log(`Movie with ID ${movieId} not found.`); 
+        res.status(404).send("Not found")
+    }
+})
